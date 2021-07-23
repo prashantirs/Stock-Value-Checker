d33ff06871baed4ddd28defd4672271fb4aab837
Prashant Srivastava <prashantsrivastava381@gmail.com>
2021-07-23 13:44:04 +0530

    BSE API First Version

---
+++ index.js
@@ -0,0 +1,59 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+    });
+
+
+app.post("/",function(req,res){
+     
+    var stockSymbol= (req.body.stocks).toString();
+   
+    
+    var baseURL="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=";
+    var endingURL=".BSE&outputsize=full&apikey=OVQ5IPXU8T4QB29Q";
+    var finalURL=baseURL+stockSymbol+endingURL;
+
+
+    
+
+    request(finalURL,function(error,response,body){
+         var data=JSON.parse(body); 
+         var dating=req.body.date;
+
+         var jpath=data['Time Series (Daily)'][dating]['4. close'];
+      
+         var price=jpath;  
+         // var stockSymbol= (req.body.stocks).toString();  
+        // res.send(stockSymbol+ " price is "+price+" Rupees On "+dating);
+
+        if (error) {
+            res.sendFile(__dirname + "/failure.html");
+          } else {
+            if (response.statusCode == 200) {
+                res.send(stockSymbol+ " Price was ₹"+price+" on "+dating);
+            } else{
+              res.sendFile(__dirname + "/failure.html");
+            }
+         
+          }
+    });
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT ||3000,function(){
+    console.log("Server is running");
+});
